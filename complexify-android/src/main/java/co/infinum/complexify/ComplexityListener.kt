75c3939ad1d1a0156b1fa8cb81b5f1da25adcccf
Jonatan Pleško <jonatan.plesko@infinum.hr>
2020-12-24 14:50:38 +0100

    ComplexityListener to Kotlin

---
+++ complexify-android/src/main/java/co/infinum/complexify/ComplexityListener.kt
@@ -0,0 +1,15 @@
+package co.infinum.complexify
+
+/**
+ * Password complexity calculation listener
+ */
+interface ComplexityListener {
+
+    /**
+     * Called when the password complexity calculation is done.
+     *
+     * @param isValid    true if the password is valid, false otherwise
+     * @param complexity A number from range [0.0, 100.0] where greater number represents greater complexity
+     */
+    fun onSuccess(isValid: Boolean, complexity: Double)
+}
