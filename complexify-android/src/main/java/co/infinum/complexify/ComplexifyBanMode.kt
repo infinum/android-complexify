dc871d00bdfad5ce900f7a67c11f2eeb6e1433b4
Jonatan Pleško <jonatan.plesko@infinum.hr>
2020-12-24 14:52:25 +0100

    ComplexifyBanMode to Kotlin

---
+++ complexify-android/src/main/java/co/infinum/complexify/ComplexifyBanMode.kt
@@ -0,0 +1,16 @@
+package co.infinum.complexify
+
+/**
+ * Which ban mode to use when using the [Complexify.banList].
+ */
+enum class ComplexifyBanMode {
+    /**
+     * Don't allow a substring of a banned password.
+     */
+    STRICT,
+
+    /**
+     * Only ban exact matches of a banned password.
+     */
+    LOOSE
+}
