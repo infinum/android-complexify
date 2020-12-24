95ca912fb8bfc0b81b3c27753753ce4434729847
Jonatan Pleško <jonatan.plesko@infinum.hr>
2020-12-24 14:07:58 +0100

    ComplexifyExecuteMode to Kotlin

---
+++ complexify-android/src/main/java/co/infinum/complexify_android/ComplexifyExecuteMode.kt
@@ -0,0 +1,11 @@
+package co.infinum.complexify_android
+
+/**
+ * Enum for execution mode.
+ *
+ * SYNC - execution is synchronous
+ * ASYNC - execution is asynchronous
+ */
+enum class ComplexifyExecuteMode {
+    SYNC, ASYNC
+}
